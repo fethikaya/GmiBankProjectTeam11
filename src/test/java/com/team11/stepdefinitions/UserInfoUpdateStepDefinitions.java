2b1073d6d5e9452ce38cd1baf3aafe4498e672c3
fatih ustunyer <fustunyer1@gmail.com>
2020-11-14 23:50:10 +0300

    calisma1

--- src/test/java/com/team11/stepdefinitions/UserInfoUpdateStepDefinitions.java
+++ src/test/java/com/team11/stepdefinitions/UserInfoUpdateStepDefinitions.java
@@ -1,4 +1,23 @@
 package com.team11.stepdefinitions;
 
+import com.team11.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class UserInfoUpdateStepDefinitions {
+
+    public UserInfoUpdateStepDefinitions(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+    @FindBy(id = "email")
+    public WebElement userInfoUpdateEmailBox;
+    @FindBy(id = "langKey")
+    public WebElement userInfoUpdateLanguageBox;
+
+
+
+
+
+
 }
