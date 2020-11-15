491dde7c65d767fc3a92a3ea599279b42f2b4f76
ozlemdeveci <ozlemelisss@gmail.com>
2020-11-14 22:02:49 -0600

    ozlem2 deki runner ve step deflerin ibrahim hocaylayazdik

--- src/test/java/com/team11/pages/GmiRegistirationPage.java
+++ src/test/java/com/team11/pages/GmiRegistirationPage.java
@@ -1,4 +1,18 @@
 package com.team11.pages;
 
+import com.team11.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class GmiRegistirationPage {
+    public GmiRegistirationPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(id = "ssn")
+    public WebElement ssnTextbox;
+
+    @FindBy(xpath = "//*[@class='invalid-feedback']")
+    public WebElement ssnMessage;
 }
