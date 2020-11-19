ed1c1f135f0923f7a7fe748f97513b38429505fa
fustunyer <67637972+fustunyer@users.noreply.github.com>
2020-11-19 23:43:51 +0300

    Update Runner.java

--- src/test/java/com/team11/runners/Runner.java
+++ src/test/java/com/team11/runners/Runner.java
@@ -5,16 +5,14 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(strict = true,
+@CucumberOptions(
         plugin = {"html:target/default-cucumber-reports",
                 "json:target/json-reports/cucumber.json",
                 "junit:target/xml-report/cucumber.xml"},
         features = "src/test/resources/features",
         glue = "com/team11/stepdefinitions",
-        tags = "@applicant12",
+        tags = "",
         dryRun = false
-
-        
 )
 public class Runner {
 
