1fbe789529502fa8d9dd9a86477f2f47da78d4f6
fatih ustunyer <fustunyer1@gmail.com>
2020-11-20 00:33:47 +0300

    en yeni master

--- src/test/java/com/team11/stepdefinitions/ChoosingDateStepDefinitions.java
+++ src/test/java/com/team11/stepdefinitions/ChoosingDateStepDefinitions.java
@@ -1,9 +1,8 @@
 package com.team11.stepdefinitions;
 
-import com.team11.pages.CreateANewAccount;
+import com.team11.pages.CreateANewAccountPage;
 import com.team11.pages.EditPasswordPage;
 import com.team11.pages.GmiSignInPage;
-import com.team11.utilities.ConfigurationReader;
 import com.team11.utilities.Driver;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
@@ -11,38 +10,38 @@ import io.cucumber.java.en.Given;
 
 public class ChoosingDateStepDefinitions {
 
-    CreateANewAccount createANewAccount = new CreateANewAccount();
+    CreateANewAccountPage createANewAccountPage = new CreateANewAccountPage();
     GmiSignInPage gmiSignInPage = new GmiSignInPage();
     EditPasswordPage editPasswordPage =new EditPasswordPage();
 
 
     @Given("Click on My Operations dropdown menu")
     public void click_on_My_Operations_dropdown_menu() {
-       Driver.waitAndClick(createANewAccount.myOperations,3);
+       Driver.waitAndClick(createANewAccountPage.myOperations,3);
     }
 
 
     @And("Click on Manage Accounts")
     public void clickOnManageAccounts() {
-        Driver.waitAndClick(createANewAccount.manageAccounts,3);
+        Driver.waitAndClick(createANewAccountPage.manageAccounts,3);
     }
 
     @And("Click on +Create a new Account")
     public void clickOnCreateANewAccount() {
-        Driver.waitAndClick(createANewAccount.newAccount,3);
+        Driver.waitAndClick(createANewAccountPage.newAccount,3);
     }
 
 
     @And("Click on the Create Date input box and enter the {string}")
     public void clickOnTheCreateDateInputBoxAndEnterThe(String creatingDate) {
-        Driver.waitAndSendText(createANewAccount.createDate,creatingDate,3);
+        Driver.waitAndSendText(createANewAccountPage.createDate,creatingDate,3);
     }
 
 
     @And("Click on the Closed Date input box and enter the  {string} as later than the time of {string}")
     public void clickOnTheClosedDateInputBoxAndEnterTheAsLaterThanTheTimeOf(String closingDate, String creatingDate) {
 
-        createANewAccount.enterDate(closingDate,creatingDate);
+        createANewAccountPage.enterDate(closingDate,creatingDate);
 
 
         }
--- src/test/java/com/team11/stepdefinitions/CreaeNewAccountStepDefinitions.java
+++ src/test/java/com/team11/stepdefinitions/CreaeNewAccountStepDefinitions.java
@@ -1,10 +1,5 @@
 package com.team11.stepdefinitions;
 
-import com.team11.pages.CreateANewAccount;
-import com.team11.utilities.ConfigurationReader;
-import com.team11.utilities.Driver;
-import io.cucumber.java.en.And;
-import io.cucumber.java.en.Given;
 public class CreaeNewAccountStepDefinitions {
 
 
